291e55292c47d493eff57c3574cdb25aae1326ca
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-07-11 12:18:28 -0700

    Remove css transition

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 (function () {
   'use strict';
   window.addEventListener('keydown', function(e) {
-    console.log(e.keyCode);
-    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`)
-    console.log(audio);
+    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+    const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
 
     if (!audio) return;  // stop function from running altogether
     audio.currentTime = 0;  // rewind to start
     audio.play();
+    key.classList.add('playing');
 
-  })
+
+  });
+
+  function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;  // skip if not transform
+    console.log(this);
+    this.classList.remove('playing');
+
+  }
+
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
 
 
 })();
