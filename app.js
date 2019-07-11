bb329d9748933c3d85b5a77db39e02039fb36bb8
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-07-11 11:46:35 -0700

    Add audio

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 (function () {
   'use strict';
+  window.addEventListener('keydown', function(e) {
+    console.log(e.keyCode);
+    const audio = document.querySelector(`audio[data-key='${e.keyCode}']`)
+    console.log(audio);
 
+    audio.play();
+
+  })
 
 
 })();
