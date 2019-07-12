05643b64d64f1d521c8b70a6a68ef1c1272a82a0
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-07-11 20:00:40 -0700

    Refactor functions

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 (function () {
   'use strict';
-  window.addEventListener('keydown', function(e) {
+
+  function playSound(e) {
     const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
     const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+    if (!audio) return;  // stop function from running altogether for other key presses
 
-    if (!audio) return;  // stop function from running altogether
-    audio.currentTime = 0;  // rewind to start
+    key.classList.add('playing');  // add css transition
+    audio.currentTime = 0;  // rewind to start for instant replays
     audio.play();
-    key.classList.add('playing');
-
-
-  });
+  }
 
   function removeTransition(e) {
     if (e.propertyName !== 'transform') return;  // skip if not transform
-    console.log(this);
     this.classList.remove('playing');
-
   }
+
+  window.addEventListener('keydown', function(e) {
+    playSound(e);
+  });
 
   const keys = document.querySelectorAll('.key');
   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-
 
 
 })();
